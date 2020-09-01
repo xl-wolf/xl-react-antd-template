0ba8c65466b563363f3a9806eae51b8bdbd84618
xl-wolf <1072236984@qq.com>
2020-09-01 15:42:20 +0800

    react 中不能直接操作源数据，需要对新数据操作才可以重新渲染界面

--- src/views/hookUseContext/Button.jsx
+++ src/views/hookUseContext/Button.jsx
@@ -1,23 +1,70 @@
-import React, { useContext } from 'react'
+import React, {
+  useState,
+  //  useEffect,
+  useContext
+} from 'react'
 import { UPDATE_COLOR } from './redux/action-types'
 import { store } from './redux/store'
 import { Button } from "antd";
 export default function () {
+  let [btns, setBtns] = useState([])
+  let [addTimes, setAddTimes] = useState(0)
+
   const { dispatch } = useContext(store)
+
   function changeColor(type, color) {
     dispatch({ type, color })
   }
+
+  function addBtn() {
+    const btnArray = [
+      { id: 1, type: UPDATE_COLOR, color: 'orange' },
+      { id: 2, type: UPDATE_COLOR, color: 'red' },
+      { id: 3, type: UPDATE_COLOR, color: 'cyan' }
+    ]
+    if (addTimes < 3) {
+      btns.push(btnArray[addTimes])
+      const btnArr = [...btns]
+      setBtns(btnArr)
+      setAddTimes(addTimes+1)
+      console.log(addTimes, btns)
+    }
+  }
+
+  function minusBtn() {
+    if (addTimes > 0) {
+      btns.pop()
+      const btnArr = [...btns]
+      setBtns(btnArr)
+      setAddTimes(addTimes-1)
+    }
+  }
+  
   return (
     <div>
-      <Button
-        type="primary"
-        onClick={() => changeColor(UPDATE_COLOR, "orange")}
-      >Orange</Button>
-      <Button
-        type="plain"
-        style={{ marginLeft: '12px' }}
-        onClick={() => changeColor(UPDATE_COLOR, "red")}
-      >Green</Button>
+      {btns.map((btn, index) => {
+        return <Button
+          key={btn.id}
+          type="plain"
+          style={{ marginLeft: index && '12px' }}
+          onClick={() => changeColor(btn.type, btn.color)}
+        >{btn.color}</Button>
+      })}
+
+      <div style={{ marginTop: '8px' }}>
+
+        <Button
+          type="primary"
+          onClick={() => addBtn()}
+        >add</Button>
+
+        <Button
+          type="primary"
+          style={{ marginLeft: '12px' }}
+          onClick={() => minusBtn()}
+        >minus</Button>
+
+      </div>
     </div>
   )
 
